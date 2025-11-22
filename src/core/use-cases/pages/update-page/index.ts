56da7e4a5bdb18d58bc44d56e3fef34f3efa6d5f
vitingr <vitorgabrielsbo1460@gmail.com>
2025-11-22 19:50:18 -0300

    fix(core): adjust update page logic

--- src/core/use-cases/pages/update-page/index.ts
+++ src/core/use-cases/pages/update-page/index.ts
@@ -3,21 +3,28 @@ import { PagesRepository } from '@/core/ports/repositories/prisma/pages-reposito
 import { UpdatePageUseCasePayload, UpdatePageUseCaseReturn } from './types'
 import { PageDoesNotExistError } from '@/core/domain/exceptions/pages'
 
+import { deepMerge } from '@/shared/utils/helpers/deep-merge'
+
 export class UpdatePageUseCase {
   constructor(protected readonly pagesRepository: PagesRepository) {}
 
   execute = async (
     payload: UpdatePageUseCasePayload
   ): Promise<UpdatePageUseCaseReturn> => {
-    const { id } = payload
+    const { id, sections } = payload
 
-    const pageExists = await this.pagesRepository.getPageById(id)
+    const currentPage = await this.pagesRepository.getPageById(id)
 
-    if (!pageExists) {
+    if (!currentPage) {
       throw new PageDoesNotExistError()
     }
 
-    const updatedPage = await this.pagesRepository.updatePage(id, payload)
+    const mergedSections = deepMerge(currentPage.sections, sections)
+
+    const updatedPage = await this.pagesRepository.updatePage(id, {
+      ...payload,
+      sections: mergedSections
+    })
 
     return {
       page: updatedPage
